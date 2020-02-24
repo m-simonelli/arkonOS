6ae6c8655221643def0110df75b5c2088e42b7a1
Marco Simonelli <msimonelli@protonmail.com>
2020-02-24 23:53:25 +1100

    Fix panic function
    Panicking now has _panic_printf to print registers to the current char
    printing handler
    Actually pass the va_list to vsprintf instead of NULL

--- kernel/util/panic.c
+++ kernel/util/panic.c
@@ -8,39 +8,46 @@
  * we can still print regs */
 struct panic_dump p_regs = {0};
 
+void _panic_printf(char *msg, ...){
+    va_list(ap);
+    va_start(ap, msg);
+    vsprintf(NULL, log_putchar, msg, ap);
+    va_end(ap);
+}
+
 /* panic and halt */
 __attribute__((noreturn)) void _panic(char *pmsg, ...) {
     va_list(ap);
     va_start(ap, pmsg);
     vsprintf(NULL, log_putchar, "\n*****PANIC*****\n", NULL);
-    vsprintf(NULL, log_putchar, pmsg, NULL);
+    vsprintf(NULL, log_putchar, pmsg, ap);
     vsprintf(NULL, log_putchar, "\nRegister Dump:\n", NULL);
 #ifdef KERN_TARGET_x86_64
-    vga_printf("RIP: %#0*llx\n", 16, p_regs.ip);
+    _panic_printf("RIP: %#0*llx\n", 16, p_regs.ip);
 
-    vga_printf("RAX: %#0*llx\n", 16, p_regs.ax);
-    vga_printf("RBX: %#0*llx\n", 16, p_regs.bx);
-    vga_printf("RCX: %#0*llx\n", 16, p_regs.cx);
-    vga_printf("RDX: %#0*llx\n", 16, p_regs.dx);
+    _panic_printf("RAX: %#0*llx\n", 16, p_regs.ax);
+    _panic_printf("RBX: %#0*llx\n", 16, p_regs.bx);
+    _panic_printf("RCX: %#0*llx\n", 16, p_regs.cx);
+    _panic_printf("RDX: %#0*llx\n", 16, p_regs.dx);
 
-    vga_printf("RSI: %#0*llx\n", 16, p_regs.si);
-    vga_printf("RDI: %#0*llx\n", 16, p_regs.di);
+    _panic_printf("RSI: %#0*llx\n", 16, p_regs.si);
+    _panic_printf("RDI: %#0*llx\n", 16, p_regs.di);
 
-    vga_printf("RSP: %#0*llx\n", 16, p_regs.sp);
-    vga_printf("RBP: %#0*llx\n", 16, p_regs.bp);
+    _panic_printf("RSP: %#0*llx\n", 16, p_regs.sp);
+    _panic_printf("RBP: %#0*llx\n", 16, p_regs.bp);
 
 
-    vga_printf("R8: %#0*llx\n", 16, p_regs.r8);
-    vga_printf("R9: %#0*llx\n", 16, p_regs.r9);
-    vga_printf("R10: %#0*llx\n", 16, p_regs.r10);
-    vga_printf("R11: %#0*llx\n", 16, p_regs.r11);
-    vga_printf("R12: %#0*llx\n", 16, p_regs.r12);
-    vga_printf("R13: %#0*llx\n", 16, p_regs.r13);
-    vga_printf("R14: %#0*llx\n", 16, p_regs.r14);
-    vga_printf("R15: %#0*llx\n", 16, p_regs.r15);
+    _panic_printf("R8: %#0*llx\n", 16, p_regs.r8);
+    _panic_printf("R9: %#0*llx\n", 16, p_regs.r9);
+    _panic_printf("R10: %#0*llx\n", 16, p_regs.r10);
+    _panic_printf("R11: %#0*llx\n", 16, p_regs.r11);
+    _panic_printf("R12: %#0*llx\n", 16, p_regs.r12);
+    _panic_printf("R13: %#0*llx\n", 16, p_regs.r13);
+    _panic_printf("R14: %#0*llx\n", 16, p_regs.r14);
+    _panic_printf("R15: %#0*llx\n", 16, p_regs.r15);
 
-    vga_printf("CR2: %#0*llx\n", 16, p_regs.cr2);
-    vga_printf("CR3: %#0*llx\n", 16, p_regs.cr3);
+    _panic_printf("CR2: %#0*llx\n", 16, p_regs.cr2);
+    _panic_printf("CR3: %#0*llx\n", 16, p_regs.cr3);
 #else  /* !KERN_TARGET_x86_64 */
     /* TODO: 32 bit panic reg dumping */
 #endif /* KERN_TARGET_x86_64 */
