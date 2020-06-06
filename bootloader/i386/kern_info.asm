bf9471f41e8a1046b9fa96c88d3b88de4fbbf476
Marco Simonelli <msimonelli@protonmail.com>
2020-06-07 01:52:38 +1000

    remove build products

--- bootloader/i386/kern_info.asm
+++
@@ -1,8 +0,0 @@
-%ifndef _boot_kern_info_asm
-%define _boot_kern_info_asm
-%define KERN_LOAD_ADDR 0x100000
-%define KERN_SECTOR_COUNT 67
-%define KERN_SEGMENT_ADDR 0x10000
-%define KERN_SEGMENT_OFF 0
-%define kernel_vaddr 0xFFFFFFFFC0000000
-%endif
--- kernel/include/conf.h
+++
@@ -1,9 +0,0 @@
-#ifndef _kern_conf_h
-#define _kern_conf_h
-#define KERN_TARGET_x86_64
-#define KERN_TARGET_QEMU 1
-#define KERN_QEMU_DEBUG_PORT_ENABLED 1
-#define LOG_TO_SERIAL 1
-#define KERN_LOAD_ADDR 0x100000
-#define KERN_VADDR 0xFFFFFFFFC0000000
-#endif /* _kern_conf_h */
